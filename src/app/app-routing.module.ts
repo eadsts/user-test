ecf8e196c4e9069c67ea21334a373d544834d3c9
Tracy Eads <tracyeads1@yahoo.com>
2020-11-11 16:30:44 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './user/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/users/list", pathMatch: "full" },
+  //starts up user list automatically when app starts
+  { path: "users/list", component: UserListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
